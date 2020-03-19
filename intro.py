d0e8b666df4e024f396fde46d08a846d543da339
tungyr-one <tungusov.victor@gmail.com>
2020-03-19 10:38:46 +0300

    moved widget modules to widgets, intro module ok

--- intro.py
+++ intro.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
-from PyQt5 import Qt
-from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QFileInfo, QSettings, QSize
 
+from PyQt5 import QtWidgets
+
+from PyQt5.QtWidgets import QWidget
+import sys
+
+from ui import ui_intro
 from ui_intro import Ui_Draft_survey
-import bealuna_eng
-import bealuna_rus
-import anyvsl_eng
-import anyvsl_rus
+from widgets import anyvsl_eng, anyvsl_rus, bealuna_eng, bealuna_rus
+
 
 class IntroWindow(QWidget):
     def __init__(self, parent=None):
         """Начальное окно с опциями выбора дальнейшей модели расчета"""
         super(IntroWindow, self).__init__(parent)
-        # self.setObjectName("MainWindow")
-        # self.setMinimumSize(QSize(400, 300))
-        # self.setWindowTitle("Draft Survey")
 
         self.ui_intro = Ui_Draft_survey()
-        self.ui_intro.setupUi(self)  # использование модуля с настройками интерфейса программы
+        # использование модуля с настройками интерфейса программы
+        self.ui_intro.setupUi(self)
 
         # запуск основного окна в зависимости от нажатой кнопки
         self.ui_intro.letsgo_btn.clicked.connect(self.launch_main_window)
 
-    def launch_main_window(self):
+    def launch_main_window(self) -> None:
         if self.ui_intro.eng_radiobtn.isChecked() and self.ui_intro.bealuna_radiobtn.isChecked():
             self.main_win = bealuna_eng.MainWindowEng()
             self.main_win.show()
